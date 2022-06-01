4db162e86dde2f330ebe826c4d9fca5459da1c73
mohkabir <kbshowkb@gmail.com>
2022-06-01 11:36:34 +0100

    updated

--- src/modules/PrivateArea/ResourceManagement/ResourceHeader.js
+++ src/modules/PrivateArea/ResourceManagement/ResourceHeader.js
@@ -4,7 +4,7 @@ const ResourceHeader = ({ currentView }) => {
   return (
     <div className="resourceHeader">
       <div className="container">
-        <h3>ResourceHeader</h3>
+        <h3>Resource Center</h3>
         {currentView === 1 ? "" : <button>+ Create New Post</button>}
       </div>
     </div>
