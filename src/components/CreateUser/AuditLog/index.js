3c0bfbc79ed42e906de506906efb83f4308517a2
mohkabir <kbshowkb@gmail.com>
2022-06-03 20:17:28 +0100

    updated the audit log page

--- src/components/CreateUser/AuditLog/index.js
+++ src/components/CreateUser/AuditLog/index.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AuditLog.scss";
 
 const AuditLog = () => {
+  const [showBubble, setShowBubble] = useState(false);
+
+    const handleOpenModal = (type) => {
+      // setShowModal(true);
+      // setUserType(type);
+      setShowBubble(false);
+    };
+
+
   const auditLogDatas = [
     {
       userName: "salam sabutuwete",
@@ -47,7 +56,52 @@ const AuditLog = () => {
           <div>
             <h3>AuditLog</h3>
           </div>
-          <div>Export Audit Log</div>
+          <div className="dropdown_bubble">
+            <div
+              className="exportLog"
+              onClick={() => setShowBubble(!showBubble)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="17.659"
+                height="19.622"
+                viewBox="0 0 17.659 19.622"
+              >
+                <g id="export" transform="translate(-3.384)">
+                  <path
+                    id="Path_6992"
+                    data-name="Path 6992"
+                    d="M17.755,23.348H14.823v1.74h2.932c.872,0,1.548.445,1.548.829v7.715c0,.383-.676.829-1.548.829H6.672c-.872,0-1.548-.445-1.548-.829V25.917c0-.383.676-.829,1.548-.829H9.6v-1.74H6.672c-1.844,0-3.288,1.128-3.288,2.568v7.715c0,1.441,1.444,2.568,3.288,2.568H17.755c1.844,0,3.288-1.128,3.288-2.568V25.917C21.043,24.476,19.6,23.348,17.755,23.348Z"
+                    transform="translate(0 -16.578)"
+                    fill="#707070"
+                  />
+                  <path
+                    id="Path_6993"
+                    data-name="Path 6993"
+                    d="M22.826,4.336a.867.867,0,0,0,.615-.255L24.53,2.992v8.85a.87.87,0,1,0,1.74,0V2.934l1.148,1.148a.87.87,0,1,0,1.23-1.23l-2.6-2.6A.866.866,0,0,0,25.436,0h-.013a.866.866,0,0,0-.615.254l-2.6,2.6a.87.87,0,0,0,.615,1.485Z"
+                    transform="translate(-13.187)"
+                    fill="#707070"
+                  />
+                </g>
+              </svg>
+              <span>Export Audit Log</span>
+            </div>
+            <ul
+              className="dropdown_menu"
+              style={{ display: showBubble ? "block" : "none" }}
+              aria-labelledby="dLabel"
+            >
+              <li onClick={() => handleOpenModal()}>
+                <span>As CSV</span>
+              </li>
+              <li onClick={() => handleOpenModal()}>
+                <span>As Excel</span>
+              </li>
+              <li onClick={() => handleOpenModal()}>
+                <span>As PDF</span>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="auditBody">
           <div className="auditLogHead auditLogGrid">
@@ -70,8 +124,8 @@ const AuditLog = () => {
               <h4>{auditLogData.ipAddress}</h4>
               <h4>{auditLogData.activity}</h4>
               <h4
-                      style={{
-                    fontWeight:"600",
+                style={{
+                  fontWeight: "600",
                   color:
                     auditLogData.status === "success" ? "#23980E" : "#FF2020",
                 }}
