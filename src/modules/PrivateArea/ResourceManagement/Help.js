0fc2cf8364711f2736201f6811f53026eb715e20
mohkabir <kbshowkb@gmail.com>
2022-06-08 21:18:17 +0100

    added tootip to help tips components resource management

--- src/modules/PrivateArea/ResourceManagement/Help.js
+++ src/modules/PrivateArea/ResourceManagement/Help.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import image1 from "../../../assets/images/image1.jpg";
 import checkedIcon from "../../../assets/images/checked-icon.svg";
 
 import editIcon from "../../../assets/images/edit-icon.svg";
 
 const Help = () => {
+    const [showBubble, setShowBubble] = useState(false);
   const helpCards = [
     {
       text: "Donec dapibus mauris id odio ornare tempus. Duis sit amet accumsan justo, quis tempor ligula.",
@@ -47,6 +48,13 @@ const Help = () => {
       link: "",
     },
   ];
+
+    const handleOpenModal = (type) => {
+      // setShowModal(true);
+      // setUserType(type);
+      setShowBubble(false);
+    };
+
   return (
     <div className="helpCards">
       {helpCards.map((helpCard, index) => (
@@ -61,7 +69,18 @@ const Help = () => {
                   View More
                   <img className="editIcon" src={editIcon} alt="editIcon" />
                 </strong>
-                <span>...</span>
+                <div className="dropdown_bubble">
+                    <ul
+                      className="dropdown_menu"
+                      style={{ display: showBubble ? "block" : "none" }}
+                      aria-labelledby="dLabel"
+                    >
+                      <li>edit help tip</li>
+                      <li>delete help tip</li>
+                    </ul>
+                  </div>
+                  <span onClick={() => setShowBubble(!showBubble)}>...</span>
+              
               </div>
             </div>
           </div>
