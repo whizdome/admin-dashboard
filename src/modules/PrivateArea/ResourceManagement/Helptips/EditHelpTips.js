f734b376697d03e77575b999dc17b93dbdd8ed48
mohkabir <kbshowkb@gmail.com>
2022-06-13 22:52:07 +0100

    created helptip view and edit page

--- src/modules/PrivateArea/ResourceManagement/Helptips/EditHelpTips.js
+++ src/modules/PrivateArea/ResourceManagement/Helptips/EditHelpTips.js
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const EditHelpTips = () => {
+import { Select } from "antd";
+const { Option, OptGroup } = Select;
+
+const EditHelpTips = ({ showModal }) => {
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+
   return (
-    <div>EditHelpTips</div>
-  )
-}
+    <div className="editHelpTips">
+      <form>
+        <div className="subject">
+          <h3>Add a Subject</h3>
+          <input type="text" placeholder="Type in a Subject" />
+        </div>
+        <div className="subject">
+          <h3>Add a Category</h3>
+          <Select
+            defaultValue="lucy"
+            style={{
+              width: "100%",
+            }}
+            onChange={handleChange}
+          >
+            <option value="---Choose Category---">---Choose Category---</option>
+            <option value="About Oasis">About Oasis</option>
+            <option value="E-Dividend claim">E-Dividend claim</option>
+            <option value="Demilitiarization">Demilitiarization</option>
+            <option value="Share Transfer">Share Transfer</option>
+            <option value="Probate and Transmission">
+              Probate and Transmission
+            </option>
+            <option value="Safety and accessibility">
+              Safety and accessibility
+            </option>
+          </Select>
+        </div>
 
-export default EditHelpTips
+        {/* <div className="message">
+          <h3>Message</h3>
+        </div> */}
+
+        <div className="upload_img">
+          <h3 style={{ color: "#D90B2C" }}>Upload Cover Image</h3>
+        </div>
+        <div className="possible_question">
+          <div className="head">
+            <h3>Possible Question</h3>
+            <h3 style={{ color: "#D90B2C" }}>+ Add Question</h3>
+          </div>
+          <div>
+            <h3>Question</h3>
+            <input type="text" placeholder="Type in Question" />
+          </div>
+          <div>
+            <h3>Answer</h3>
+            <input type="text" placeholder="Type in Your Answer" />
+          </div>
+        </div>
+
+        <div className="submit">
+          <h3 onClick={showModal}>Preview help tip</h3>
+          <div>
+            <button type="button">
+              <Link to="/resource-management/help-tip/1">Cancel</Link>
+            </button>
+
+            <button type="submit">
+              <Link to="/resource-management/help-tip/1">Update Help Tip</Link>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default EditHelpTips;
---
+++ src/modules/PrivateArea/ResourceManagement/Helptips/ViewHelpTips.js
@@ -0,0 +1,71 @@
+import React from "react";
+import about1 from "../../../../assets/images/about1.jpg";
+
+import { Collapse } from "antd";
+const { Panel } = Collapse;
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
+export const Accord = () => {
+  const onChange = (key) => {
+    console.log(key);
+  };
+
+  const accordionDatas = [
+    {
+      header: "Nunc dictum lectus sem, vel dignissim.1",
+      text: "",
+    },
+    {
+      header: "Nunc dictum lectus sem, vel dignissim.2",
+      text: "",
+    },
+    {
+      header: "Nunc dictum lectus sem, vel dignissim.3",
+      text: "",
+    },
+    {
+      header: "Nunc dictum lectus sem, vel dignissim.4",
+      text: "",
+    },
+  ];
+  return (
+    <Collapse defaultActiveKey={["0"]} onChange={onChange}>
+      {accordionDatas.map((data, index) => (
+        <Panel header={data.header} key={index}>
+          <p>{text}</p>
+        </Panel>
+      ))}
+    </Collapse>
+  );
+};
+
+const ViewHelpTips = () => {
+  return (
+    <div className="viewHelpTips">
+      <h4>Nunc dictum lectus sem, vel dignissim</h4>
+      <div className="img_cover">
+        <img src={about1} alt="about1" />
+      </div>
+      <p>
+        Mauris neque nisi, faucibus non elementum in, convallis et eros. Sed
+        pretium sem libero, vel pellentesque purus ultrices ut. In quis leo id
+        massa pulvinar euismod cursus non justo. Sed sagittis et urna non
+        efficitur. Nulla nec lacus tincidunt, rutrum arcu in, euismod diam.
+        Donec neque tellus, congue sed velit sed, scelerisque scelerisque urna.
+        Praesent mi sem, tincidunt eget facilisis in, pharetra et sapien. Proin
+        sagittis erat magna, id eleifend ante posuere nec. Suspendisse potenti.
+        Suspendisse tincidunt sed tortor at porta. Donec a molestie lectus, ac
+        laoreet tellus. Nullam non rutrum velit, in lacinia diam. Nam vulputate
+        elit sit amet orci mattis faucibus. Nam auctor eu eros in vehicula.
+        Donec non risus id lacus aliquet.
+      </p>
+      <Accord />
+    </div>
+  );
+};
+
+export default ViewHelpTips;
